682e273c66cace4d058c519c68a01f6d0196e494
Prafulkumar Patel <contact2praful@gmail.com>
2020-06-03 01:24:21 +0530

    Corrected Startup and program files

--- PromotionalEngineAPI/Program.cs
+++ PromotionalEngineAPI/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace PromotionalEngineAPI
 {
@@ -13,7 +15,16 @@ namespace PromotionalEngineAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Information("Starting Host. App Version: {Version}",
+                    Assembly.GetEntryAssembly()?.GetName().Version);
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- PromotionalEngineAPI/Startup.cs
+++ PromotionalEngineAPI/Startup.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Logging;
 
 namespace PromotionalEngineAPI
 {
+    [ExcludeFromCodeCoverage]
     public class Startup
     {
         public Startup(IConfiguration configuration)
